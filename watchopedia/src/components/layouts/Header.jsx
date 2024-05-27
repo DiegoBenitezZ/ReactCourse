31bc87946f25c0fce5d41ecdf5c609913de87c69
DiegoBenzZ <diegomarcano09@gmail.com>
2024-05-27 16:48:49 -0400

    Create and clean the WatchOpedia Project

---
+++ watchopedia/src/components/layouts/Header.jsx
@@ -0,0 +1,14 @@
+import logo from '../../img/react.png'
+
+const Header = () => {
+    return (
+        <div className="py-2 pl-2 mb-4" style={{ borderBottom: "1px solid #777" }}>
+            <img src={logo} alt='React Logo' style={{height: '35px', verticalAlign: 'top'}}></img>
+            <span className='h2 pt-4 m-2 text-white-50'>
+                WatchOPedia
+            </span>
+        </div>
+    );
+}
+
+export default Header
